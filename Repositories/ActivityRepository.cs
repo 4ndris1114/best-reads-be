0716b6b0602b47b2c2deb4829da32c05c1d8596e
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-04-28 11:54:26 +0200

    fixed filtering logic

--- Repositories/ActivityRepository.cs
+++ Repositories/ActivityRepository.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Linq;
 using BestReads.Models;
 using BestReads.Database;
+using MongoDB.Bson;
 
 namespace BestReads.Repositories;
 
@@ -11,7 +12,8 @@ public class ActivityRepository : BaseRepository<Activity>{
     public ActivityRepository(MongoDbContext context) : base(context, "activities") { }
 
     public async Task<List<Activity>> GetRecentActivitiesAsync(IEnumerable<string> followingUserIds, int limit = 20) {
-        var filter = Builders<Activity>.Filter.In("UserId", followingUserIds);
+
+        var filter = Builders<Activity>.Filter.In(a => a.UserId, followingUserIds);
         var sort = Builders<Activity>.Sort.Descending(a => a.CreatedAt);
 
         var activities = await getCollection()
@@ -21,5 +23,13 @@ public class ActivityRepository : BaseRepository<Activity>{
             .ToListAsync();
 
         return activities;
+    }
+
+    public async Task AddActivityAsync(Activity activity) {
+        try {
+            await getCollection().InsertOneAsync(activity);
+        } catch (Exception ex) {
+            throw new Exception("Error adding activity", ex);
+        }
     }
 }
