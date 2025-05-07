b7611ce698c7c26362585ee516651c30a400ea75
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-05-07 16:14:44 +0200

    SignalR integration (#11)
    * added signalr package
    
    * added ActivityHub to send messages
    
    * send the message to FE
    
    * casing fix
    
    * fix casing

---
+++ Hubs/ActivityHub.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace BestReads.Hubs;
+
+public class ActivityHub : Hub {
+
+}
--- Services/ActivityService.cs
+++ Services/ActivityService.cs
@@ -3,14 +3,18 @@ using BestReads.Repositories;
 using System;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using Microsoft.AspNetCore.SignalR;
+using BestReads.Hubs;
 
 namespace BestReads.Services;
 
 public class ActivityService {
     private readonly ActivityRepository _activityRepository;
+    private readonly IHubContext<ActivityHub> _hubContext;
 
-    public ActivityService(ActivityRepository activityRepository) {
+    public ActivityService(ActivityRepository activityRepository, IHubContext<ActivityHub> hubContext) {
         _activityRepository = activityRepository;
+        _hubContext = hubContext;
     }
 
     public async Task<string?> LogBookAddedToShelfAsync(string userId, string bookId, string bookTitle, string coverImage, bool isUpdate, string? sourceShelfName, string? targetShelfName) {
@@ -21,15 +25,26 @@ public class ActivityService {
             CreatedAt = DateTime.UtcNow,
             Payload = new
             {
-                BookTitle = bookTitle,
-                CoverImage = coverImage,
-                TargetShelfName = targetShelfName,
-                SourceShelfName = isUpdate ? sourceShelfName : null,
-                IsUpdate = isUpdate
+                bookTitle = bookTitle,
+                coverImage = coverImage,
+                targetShelfName = targetShelfName,
+                sourceShelfName = isUpdate ? sourceShelfName : null,
+                isUpdate = isUpdate
             }
         };
 
-        return await _activityRepository.AddActivityAsync(activity);
+        try {
+            var id = await _activityRepository.AddActivityAsync(activity);
+
+            if (id != null) {
+                // Send the full activity to all clients
+                await _hubContext.Clients.All.SendAsync("ReceiveActivity", activity);
+            }
+            return id;
+        } catch (Exception ex) {
+            Console.WriteLine($"Error logging activity: {ex}");
+            return null;
+        }
     }
 
     public async Task<string?> LogBookReviewedAsync(string userId, string bookId, string bookTitle, string coverImage, double rating, string? reviewText, bool isUpdate) {
@@ -39,14 +54,24 @@ public class ActivityService {
             BookId = bookId,
             CreatedAt = DateTime.UtcNow,
             Payload = new {
-                BookTitle = bookTitle,
-                CoverImage = coverImage,
-                Rating = rating,
-                ReviewText = reviewText,
-                IsUpdate = isUpdate
+                bookTitle = bookTitle,
+                coverImage = coverImage,
+                rating = rating,
+                reviewText = reviewText,
+                isUpdate = isUpdate
             }
         };
+        try {
+            var id = await _activityRepository.AddActivityAsync(activity);
 
-        return await _activityRepository.AddActivityAsync(activity);
+            if (id != null) {
+                // Send the full activity to all clients
+                await _hubContext.Clients.All.SendAsync("ReceiveActivity", activity);
+            }
+            return id;
+        } catch (Exception ex) {
+            Console.WriteLine($"Error logging activity: {ex}");
+            return null;
+        }
     }
 }
